fcf66f9440034e885e4975e5b7d4315ee0b9a78e
TufanRai <thulungtufan16@gmail.com>
2025-05-24 20:34:56 +0545

    server updated

--- server/src/types/express.d.ts
+++
@@ -1,9 +0,0 @@
-import { IPayload } from "./global.types";
-
-declare global {
-    namespace Express {
-        interface Request {
-            user:IPayload
-        }
-    }
-}
--- server/src/utils/async-handler.ts
+++
@@ -1,7 +0,0 @@
-import { NextFunction, Request, RequestHandler, Response } from "express"
-
-type Handler = (req:Request,res:Response,next:NextFunction) => Promise<any>
-
-export const asyncHandler = (fn:Handler):RequestHandler =>{
-    return (req:Request,res:Response,next:NextFunction) => Promise.resolve(fn(req,res,next)).catch((err) => next(err))
-}
