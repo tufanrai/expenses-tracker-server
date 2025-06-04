9ca798507f233049e1266e8c37f7544475cdb4e5
TufanRai <thulungtufan16@gmail.com>
2025-06-04 18:57:11 +0545

    rout updated

--- src/routes/category.routes.ts
+++ src/routes/category.routes.ts
@@ -14,7 +14,7 @@ const router = express.Router();
 
 router.post("/", Authenticate([Role.USER]), create);
 router.put("/:id", Authenticate([Role.USER]), update);
-router.get("/user", Authenticate([Role.USER]), getAllUserCategory);
+router.get("/", Authenticate([Role.USER]), getAllUserCategory);
 router.get("/:id", Authenticate([Role.USER, Role.ADMIN]), getById);
 router.get("/", Authenticate([Role.ADMIN]), getAll);
 
