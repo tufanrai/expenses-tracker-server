80a1a148ea2ad51a3abfe94644a52443bfdc5b8c
TufanRai <thulungtufan16@gmail.com>
2025-06-15 16:49:54 +0545

    updated the route for the get by id controller

--- src/routes/expense.routes.ts
+++ src/routes/expense.routes.ts
@@ -1,17 +1,34 @@
-import express from 'express'
-import { create,update, getAllByUser,getAllUserExpByCategory,remove } from '../controllers/expense.controller'
-import { Authenticate } from '../middlewares/authentication.middleware'
-import { Role } from '../types/enum.types'
-import { uploader } from '../middlewares/upload.middleware'
+import express from "express";
+import {
+  create,
+  update,
+  getAllByUser,
+  getAllUserExpByCategory,
+  remove,
+} from "../controllers/expense.controller";
+import { Authenticate } from "../middlewares/authentication.middleware";
+import { Role } from "../types/enum.types";
+import { uploader } from "../middlewares/upload.middleware";
+import { getById } from "../controllers/category.controller";
 
-const upload = uploader('receipts')
-const router = express.Router()
+const upload = uploader("receipts");
+const router = express.Router();
 
-router.post('/',Authenticate([Role.USER]),upload.array('receipts',3),create)
-router.put('/:id',Authenticate([Role.USER]),upload.array('receipts',3),update)
-router.get('/',Authenticate([Role.USER]),getAllByUser)
-router.get('/:categoryId',getAllUserExpByCategory)
-router.delete('/:id',Authenticate([Role.USER]),remove)
+router.post(
+  "/",
+  Authenticate([Role.USER]),
+  upload.array("receipts", 3),
+  create
+);
+router.put(
+  "/:id",
+  Authenticate([Role.USER]),
+  upload.array("receipts", 3),
+  update
+);
+router.get("/", Authenticate([Role.USER]), getAllByUser);
+router.get("/:id", Authenticate([Role.USER]), getById);
+router.get("/:categoryId", getAllUserExpByCategory);
+router.delete("/:id", Authenticate([Role.USER]), remove);
 
-
-export default router
+export default router;
